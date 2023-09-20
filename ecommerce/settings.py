0f8b4635d40ac8df5479a0461131ec0260444063
Anirudh Suresh <113133041+anirudhsuresh21@users.noreply.github.com>
2023-09-20 23:58:13 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,8 +119,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'weshop_@outlook.com'
-EMAIL_HOST_PASSWORD = 'Djangoproject1'
+EMAIL_HOST_USER = '{your email}'
+EMAIL_HOST_PASSWORD = '{password}'
 
 
 
